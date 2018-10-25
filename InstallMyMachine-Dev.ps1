3b127500cdb527011f25153dc904512eaa8b5e1f
pmcelreavy <pmcelreavy@users.noreply.github.com>
2018-10-25 08:11:26 -0400

    Update InstallMyMachine-Dev.ps1

--- InstallMyMachine-Dev.ps1
+++ InstallMyMachine-Dev.ps1
@@ -13,8 +13,52 @@ Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\
 Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name HideFileExt -Value 0
 
 #--- Windows Subsystems/Features ---
-choco install -y Microsoft-Hyper-V-All -source windowsFeatures
-choco install -y Microsoft-Windows-Subsystem-Linux -source windowsfeatures
+#    To see the list of available features: choco list --source windowsfeatures
+$Features = `
+	'Microsoft-Hyper-V-All',`
+	'Microsoft-Windows-Subsystem-Linux',`
+	'IIS-WebServerRole',`
+	'IIS-WebServer',`
+	'IIS-HttpErrors',`
+	'IIS-ApplicationDevelopment',`
+	'IIS-NetFxExtensibility',`
+	'IIS-NetFxExtensibility45',`
+	'IIS-HealthAndDiagnostics',`
+	'IIS-HttpLogging',`
+	'IIS-Security',`
+	'IIS-URLAuthorization',`
+	'IIS-RequestFiltering',`
+	'IIS-Performance',`
+	'IIS-WebServerManagementTools',`
+	'WCF-Services45',`
+	'WCF-TCP-PortSharing45',`
+	'IIS-WindowsAuthentication',`
+	'IIS-DigestAuthentication',`
+	'IIS-StaticContent',`
+	'IIS-DefaultDocument',`
+	'IIS-DirectoryBrowsing',`
+	'IIS-WebSockets',`
+	'IIS-ApplicationInit',`
+	'IIS-ASPNET',`
+	'IIS-ASPNET45',`
+	'IIS-ASP',`
+	'IIS-ISAPIExtensions',`
+	'IIS-ISAPIFilter',`
+	'IIS-ServerSideIncludes',`
+	'IIS-BasicAuthentication',`
+	'IIS-HttpCompressionStatic',`
+	'IIS-ManagementConsole',`
+	'NetFx4-AdvSrvs',`
+	'NetFx4Extended-ASPNET45',`
+	'',`
+	'',`
+	'',`
+	'',`
+
+#--- Install Features ---
+ForEach ($FeatureName in $Features)
+{choco install -y $FeatureName -source windowsfeatures}
+
 
 #--- Define Packages to Install ---
 $Packages = `
